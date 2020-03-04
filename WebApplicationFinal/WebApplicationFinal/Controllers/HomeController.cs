cd99d44d32ce703dfee51ffb5e917e3c932a2c41
Trygve Valbø <trygvevalbo@outlook.com>
2020-03-04 13:21:40 +0100

    web app

---
+++ WebApplicationFinal/WebApplicationFinal/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace WebApplicationFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var mvcName = typeof(Controller).Assembly.GetName();
+            var isMono = Type.GetType("Mono.Runtime") != null;
+
+            ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+            ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+            return View();
+        }
+    }
+}
---
+++ WebApplicationFinal/WebApplicationFinal/Controllers/MysqlController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationFinal.Models;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace WebApplicationFinal.Controllers
+{
+    public class MysqlController : Controller
+    {
+        // GET: Mysql
+        public ActionResult Index()
+        {
+            List<MysqlClass> list1 = new List<MysqlClass>();
+            string mainconn = ConfigurationManager.ConnectionStrings["app2000"].ConnectionString;
+            MySqlConnection mysql = new MySqlConnection(mainconn);
+          
+            string query = "SELECT * FROM admin";
+            MySqlCommand comm = new MySqlCommand(query);
+            comm.Connection = mysql;
+            mysql.Open();
+            MySqlDataReader dr = comm.ExecuteReader();
+            while (dr.Read()) {
+                list1.Add(new MysqlClass
+                {
+                    ID_admin = dr["ID_admin"].ToString(),
+                    Username = dr["Username"].ToString(),
+                    Password = dr["password"].ToString(),
+
+                }); 
+            }
+            mysql.Close();
+            return View(list1);
+        }
+    }
+}

51f1d149fa0cfb6085e07ca5ac2a93cee643043e
Trygve <47280030+trygvetest1@users.noreply.github.com>
2020-03-11 15:39:25 +0100

    login form

--- WebApplicationFinal/WebApplicationFinal/Controllers/AccountController.cs
+++ WebApplicationFinal/WebApplicationFinal/Controllers/AccountController.cs
@@ -7,14 +7,12 @@ using WebApplicationFinal.Models;
 using MySql.Data;
 using System.Configuration;
 using MySql.Data.MySqlClient;
+using System.Diagnostics;
 
 namespace WebApplicationFinal.Controllers
 {
     public class AccountController : Controller
     {
-        
-        // GET: Account
-       // [HttpGet]
         public ActionResult Login()
         {
             return View();
@@ -26,15 +24,15 @@ namespace WebApplicationFinal.Controllers
         }
 
         [HttpPost]
-        public ActionResult Verify(Account acc)
+        public ActionResult LoginVerify(Account acc)
         {
             string mainconn = ConfigurationManager.ConnectionStrings["app2000"].ConnectionString;
             MySqlConnection mysql = new MySqlConnection(mainconn);
             string query = "SELECT * FROM user where username='"+acc.Name+"' and password='"+acc.Password+"'";
             MySqlCommand comm = new MySqlCommand(query);
             comm.Connection = mysql;
-                mysql.Open();
-                MySqlDataReader dr = comm.ExecuteReader();
+            mysql.Open();
+            MySqlDataReader dr = comm.ExecuteReader();
            if(dr.Read())
             {
                 mysql.Close();
@@ -45,18 +43,19 @@ namespace WebApplicationFinal.Controllers
                 mysql.Close();
                 return View("Error");
             }
-           
+
         }
+
         public ActionResult RegisterVerify(Account acc)
         {
             string mainconn = ConfigurationManager.ConnectionStrings["app2000"].ConnectionString;
             MySqlConnection mysql = new MySqlConnection(mainconn);
-            string query = "INSERT INTO user VALUES( username='" + acc.Name + "', password='" + acc.Password + "', location='" + acc.location + "', email='" + acc.email + "', full_name='" + acc.full_name + "', phone_NR='" + acc.phone_NR + "');";
+            string query = "INSERT INTO user (username, password, location, email, full_name, phone_NR) VALUES ('" + acc.Name + "', '" + acc.Password + "', '" + acc.location + "', '" + acc.email + "', '" + acc.full_name + "', '" + acc.phone_NR + "');";
             MySqlCommand comm = new MySqlCommand(query);
             comm.Connection = mysql;
             mysql.Open();
-            MySqlDataReader dr = comm.ExecuteReader();
-            if (dr.Read())
+            int dr = comm.ExecuteNonQuery();
+            if (dr!=null)
             {
                 mysql.Close();
                 return View("Create");
@@ -66,6 +65,8 @@ namespace WebApplicationFinal.Controllers
                 mysql.Close();
                 return View("Error");
             }
+
+
 
         }
     }

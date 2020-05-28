3c38a526f0afe08dfdd004b5a3a434fdb41f8792
trygvevalbo <trygvevalbo@gmail.com>
2020-05-28 12:46:41 +0200

    hei

--- WebApplicationFinal/WebApplicationFinal/Controllers/AccountController.cs
+++ WebApplicationFinal/WebApplicationFinal/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using System.Configuration;
 using MySql.Data.MySqlClient;
 using System.Web.Security;
 using System.Diagnostics;
+using System.Security.Cryptography;
 
 namespace WebApplicationFinal.Controllers
 {
@@ -375,8 +376,22 @@ namespace WebApplicationFinal.Controllers
             mysql.Close();
             MyAccount();
             return View("MyAccount");
+        }
 
+        public HashPassword()
+        {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
 
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            string savedPasswordHash = Convert.ToBase64String(hashBytes);
+            DBContext.AddUser(new User { ..., Password = savedPasswordHash });
         }
     }
 }

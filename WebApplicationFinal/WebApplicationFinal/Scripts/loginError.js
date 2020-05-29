9c28d7b4ce6e6787951ea80a72db67d36fc4aed4
trygvevalbo <trygvevalbo@gmail.com>
2020-05-29 12:54:52 +0200

    endring

---
+++ WebApplicationFinal/WebApplicationFinal/Scripts/loginError.js
@@ -0,0 +1,17 @@
+﻿console.log("fEIL PASSORD");
+var head = document.getElementsByTagName('HEAD')[0];
+
+// Create new link Element 
+var link = document.createElement('link');
+
+// set the attributes for link element  
+link.rel = 'stylesheet';
+
+link.type = 'text/css';
+
+link.href = '/Styles/shake.css';
+
+alert("Password or Username did not match");
+
+// Append link element to HTML head 
+head.appendChild(link);
